3ee084cba8270ced555b350773140de444066e41
Prithviraj0565 <130583768+Prithviraj0565@users.noreply.github.com>
2023-07-21 17:34:22 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+// declaring the variable
+
+int a;
+
+// taking input & storing in "a" variable ;;
+
+cout<< "input any number:- " <<endl;
+cin >> a;
+
+// checking the number if it is prime  or not ;;
+
+if(a%2 != 0 && a%3 != 0 && a%5 != 0){
+      cout<< "Yes its a prime number:- " <<endl;
+
+}
+else{
+
+      cout<< "No, it is not a prime number:- " <<endl;
+
+}
+
+
+
+// Created By PrithviRaj  !!!
+}
+
+
